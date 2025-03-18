99b28d3140bd7e2bce6f16945f6748be52a4c90c
EaeDave <eudaverdgs@gmail.com>
2025-03-18 10:16:40 -0300

    Objeto e função console.log() para exibição de dados no terminal/console

---
+++ aulas/aula0001/index.js
@@ -0,0 +1,8 @@
+console.log('David "Rodrigues" da Silva');  /* Imprimindo valores literais com console.log() */
+console.log("David 'Rodrigues' da Silva");  /* Strings podem ser informadas tanto com aspas simples ou duplas */
+console.log(`'David' "Rodrigues" da Silva`)  /* A crase é utilizada para declarar template strings, também é possível
+exibir no contexto aspas simples e duplas */
+
+console.log(123456);  /* Valor literal de número inteiro */
+console.log(12.5, 700.15134);  /* Valor literal de números flutuantes, dentro de console.log() é possível exibir sequências */
+console.log(15, 15.5, 'Esses dois valores são do tipo Number')  /* Tipo Number */
