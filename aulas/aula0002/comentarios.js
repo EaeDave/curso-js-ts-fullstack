a84075c135db58f7bb7facfd2147dbb96839970b
EaeDave <eudaverdgs@gmail.com>
2025-03-18 10:31:16 -0300

    Comentários de código // /* */

---
+++ aulas/aula0002/comentarios.js
@@ -0,0 +1,14 @@
+// Comentários são trechos de código que são ignorados pelo motor do JavaScript
+console.log('Execute e o comentário acima não será exibido');
+
+console.log('Linha 1');  // Comentários são feitos para facilitar o entendimento do código
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+/**
+ * Esse é um comentário
+ * de multiplas
+ * linhas
+ * ou, comentário de bloco
+ */
