dea4f15d341d811a8711299a316e5b169d2ec43c
EaeDave <eudaverdgs@gmail.com>
2025-03-18 10:50:01 -0300

    Navegador vs Node (HTML + JavaScript)

---
+++ aulas/aula0003/js/index.js
@@ -0,0 +1,4 @@
+console.log('Oi');
+// Um comentário em JavaScript
+console.log('Estou sendo exibido no console do navegador');
+alert('Olá, Mundo!');  // Retornara um erro na execução do node, porque essa função existe só no navegador
