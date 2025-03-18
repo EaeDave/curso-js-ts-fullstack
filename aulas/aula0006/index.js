2b9bfa8cb1d2f8860ea02fa4a1fdcd1deb154c09
EaeDave <eudaverdgs@gmail.com>
2025-03-18 13:25:08 -0300

    Let vs Var - Primeira diferença

---
+++ aulas/aula0006/index.js
@@ -0,0 +1,8 @@
+// ECMAScript é uma padronização do JavaScript
+var nome = 'David';
+var nome = 'Rodrigues';  // É possível redeclarar variáveis com var
+apelido = 'Dave';  // Não fazer dessa forma, cria uma variável global no sistema
+console.log(nome);
+
+let numero = 10;
+// let numero = 12;  Não é possível redeclarar variáveis com let
