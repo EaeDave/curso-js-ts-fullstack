ce08379951bee3f08c1542f2fd6d14c3083c59a8
EaeDave <eudaverdgs@gmail.com>
2025-03-19 13:26:30 -0300

    Exercício 0003 - Captura de valor com prompt() e exibição de resultado com alert()

---
+++ exercicios/ex0003/js/script.js
@@ -0,0 +1,3 @@
+const NUM_1 = parseFloat(prompt('Digite o primeiro valor da soma: '));
+const NUM_2 = parseFloat(prompt('Digite o segundo valor da soma: '));
+alert(`A soma entre ${NUM_1} e ${NUM_2} é = ${NUM_1 + NUM_2}`);
