f5d37e014faf4b181ec90d636bdd39cac300bb00
Zaphryn <eudaverdgs@gmail.com>
2025-03-29 14:49:30 -0300

    if, else if e else (2)

---
+++ aulas/aula0021/index.js
@@ -0,0 +1,13 @@
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('Sim, o número está entre zero e cinco.');
+} else if (numero >= 6 && numero <= 8){
+    console.log('O número está entre seis e oito.');
+    
+} else {
+    console.log('O número NÃO está entre zero e cinco.');
+}
+
+
+console.log('Essa linha de código será executada independente do if, pois está fora do bloco.');
