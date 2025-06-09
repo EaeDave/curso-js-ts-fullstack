62bfb5d9f01a87d0c3f9b047c7090a5e6608c3b8
EaeDave <eudaverdgs@gmail.com>
2025-06-09 09:17:42 -0300

    Babel (Transpilador de código js para js compatível com navegadores antigos) e can i use....

---
+++ aulas/aula0080/main.js
@@ -0,0 +1,12 @@
+// const nome = 'David';
+// const obj = { nome };
+// const novoObj = { ...nome };
+// console.log(novoObj);
+
+class Pessoa {
+    constructor(nome, sobrenome, idade) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+}
